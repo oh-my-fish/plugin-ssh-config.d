c11118150bee9f7675c73b08d8e8763107b374a4
Roman Inflianskas <infroma@gmail.com>
2016-03-17 14:49:00 +0300

    Handle ~/.ssh/config symlinks

--- functions/ssh-config.d.fish
+++ functions/ssh-config.d.fish
@@ -4,7 +4,7 @@
 
 function ssh-config.d -d "Update ~/.ssh/config file from files in ~/.ssh/config.d/ dir"
   if not cat (config ssh-config.d --get ssh_config) | grep -q "ssh-config.d"
-    cpbak (config ssh-config.d --get ssh_config)
+    __bak cpbak 'cp -aH' (config ssh-config.d --get ssh_config)
   end
   config ssh-config.d --get warn >(config ssh-config.d --get ssh_config)
   cat (config ssh-config.d --get ssh_config_dir)/**.config >>(config ssh-config.d --get ssh_config)

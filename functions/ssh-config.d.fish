d638176899c86ea8ee0e08a556eb5be63792e86f
Roman Inflianskas <infroma@gmail.com>
2016-03-23 10:50:16 +0300

    Create ~/.ssh/config.d/ directory if absent and move existing config to it

--- functions/ssh-config.d.fish
+++ functions/ssh-config.d.fish
@@ -3,9 +3,36 @@
 #
 
 function ssh-config.d -d "Update ~/.ssh/config file from files in ~/.ssh/config.d/ dir"
-  if not cat (config ssh-config.d --get ssh_config) | grep -q "ssh-config.d"
-    __bak cpbak 'cp -aH' (config ssh-config.d --get ssh_config)
+  set -g ssh_config (config ssh-config.d --get ssh_config)
+  set -g ssh_config_dir (config ssh-config.d --get ssh_config_dir)
+
+  function move_ssh_config_to_ssh_config_dir
+    mkdir $ssh_config_dir
+    mv $ssh_config $ssh_config_dir/base.config
+    echo "Your OpenSSH config was moved to new directory $ssh_config_dir" >&2
   end
-  config ssh-config.d --get warn >(config ssh-config.d --get ssh_config)
-  cat (config ssh-config.d --get ssh_config_dir)/**.config >>(config ssh-config.d --get ssh_config)
+
+  function ssh_config_is_handmade
+    [ -f $ssh_config ]; and [ -r $ssh_config ]; and not cat $ssh_config | grep -q "ssh-config.d"
+  end
+
+  function backup_ssh_config
+    __bak cpbak 'cp -aH' $ssh_config
+  end
+
+  function generate_ssh_config
+    config ssh-config.d --get warn >$ssh_config
+    cat $ssh_config_dir/**.config >>$ssh_config
+  end
+
+
+  if not [ -d $ssh_config_dir ]
+    move_ssh_config_to_ssh_config_dir
+  end
+
+  if ssh_config_is_handmade
+    backup_ssh_config
+  end
+
+  generate_ssh_config
 end
